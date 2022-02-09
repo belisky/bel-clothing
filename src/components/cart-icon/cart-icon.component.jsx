b739708d106b17d8e8249d56a7cbb2eed82ba0fe
bel <denoblesnobility2@gmail.com>
2022-02-09 03:00:27 +0000

    updating our app with reselect which memoizes selectors(rendered list of cart-items) to prevent unnecessary re-render of lists which may be caused by referential inequality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,16 +3,20 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggeleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-
-const CartIcon  = ({toggeleCartHidden}) => {
+const CartIcon  = ({toggeleCartHidden,itemCount}) => {
     return (
         <div className="cart-icon" onClick={toggeleCartHidden}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
          </div>
     );
 };
+
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = (dispatch) => ({
     toggeleCartHidden:()=>dispatch(toggeleCartHidden())
