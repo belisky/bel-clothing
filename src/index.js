70cc1f7add2b3fa24074d8e386d16567b3debc73
bel <denoblesnobility2@gmail.com>
2022-02-12 19:09:18 +0000

    still trying to fix white screen deployment problem

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import  {store, persistor}  from './redux/store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <PersistGate persistor={persistor}>
         <App />
         </PersistGate>
