41a31c1a85c83cab5f9980503181c88a14dca27a
bel <denoblesnobility2@gmail.com>
2022-02-14 09:21:40 +0000

    added an alert when item is added to cart and also corrected some styles on collection item footer section

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,15 +5,16 @@ import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss'
 import CustomButton from '../custom-button/custom-button.component';
 const CollectionItem = ({ item, addItem }) => {
-    const {  name, price, imageUrl } = item;
+    const { name, price, imageUrl } = item;
+    const prompt = () => alert('Item added to Cart');
     return (
         <div className='collection-item'>
             <div className="image" style={{ backgroundImage:`url(${imageUrl})`}} ></div>
             <div className="collection-footer">
                 <span className="name">{name}</span>
-                <span className="price">{price}</span>
+                <span className="price">${price}</span>
             </div>
-            <CustomButton onClick={() => addItem(item)} inverted>
+            <CustomButton onClick={() => { addItem(item);prompt()}} inverted>
                 Add to Cart
             </CustomButton>
         </div>
